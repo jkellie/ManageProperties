afff2c89a1c92d7f25eca28ebff8a25b83b3832d
JKellie <jkellie@me.com>
2017-11-16 18:46:04 -0500

    getter and setter for city

--- src/edu/seminolestate/properties/Property.java
+++ src/edu/seminolestate/properties/Property.java
@@ -38,6 +38,13 @@ public abstract class Property {
 		this.address = address;
 	}
 	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 	public String getState() {
 		return state;
 	}
