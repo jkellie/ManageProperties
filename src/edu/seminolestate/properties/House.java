d41fe6d0e222829f94ab10a898fcf6ec16e901f5
JKellie <jkellie@me.com>
2017-11-16 20:07:24 -0500

    add constructor and variable

--- src/edu/seminolestate/properties/House.java
+++ src/edu/seminolestate/properties/House.java
@@ -1,5 +1,23 @@
 package edu.seminolestate.properties;
 
 public class House extends Property {
+	private double lotSize;
+	
 
+	public House(int propertyID, String address, String city, String state, String zip, double value,
+			double squareFeet, double numberOfBaths, int numberOfBedrooms, double lotSize) {
+		super(propertyID, address, city, state, zip, value, squareFeet, numberOfBaths, numberOfBedrooms);
+		this.setLotSize(lotSize);
+	}
+	
+	public double getLotSize() {
+		return lotSize;
+	}
+	public void setLotSize(double lotSize) {
+		this.lotSize = lotSize;
+	}
+	
+	public String toString( ) {
+		return "change this";
+	}
 }
