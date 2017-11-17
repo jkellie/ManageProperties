664ac08689ca5adafc2a119675aeba980dada359
JKellie <jkellie@me.com>
2017-11-17 11:31:12 -0500

    add exception handling, edit toString method

--- src/edu/seminolestate/properties/House.java
+++ src/edu/seminolestate/properties/House.java
@@ -7,7 +7,8 @@ public class House extends Property implements Serializable {
 	private double lotSize;
 
 	public House(int propertyID, String address, String city, String state, String zip, double value,
-			double squareFeet, double numberOfBaths, int numberOfBedrooms, double lotSize) {
+			double squareFeet, double numberOfBaths, int numberOfBedrooms, double lotSize)
+					throws IllegalPropertyException {
 		super(propertyID, address, city, state, zip, value, squareFeet, numberOfBaths, numberOfBedrooms);
 		this.setLotSize(lotSize);
 	}
@@ -15,11 +16,17 @@ public class House extends Property implements Serializable {
 	public double getLotSize() {
 		return lotSize;
 	}
-	public void setLotSize(double lotSize) {
-		this.lotSize = lotSize;
+	public void setLotSize(double lotSize) throws IllegalPropertyException {
+		if (lotSize > 0)
+			this.lotSize = lotSize;
+		else
+			throw new
+			IllegalPropertyException("The lot size entered must be greater than 0.");
 	}
 	
+	@Override
 	public String toString( ) {
-		return "change this";
+		return super.toString() + " " +
+				getClass() + " [lotSize=" + lotSize + "]";
 	}
 }
