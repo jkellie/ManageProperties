4fec9fb353c099cdec4a3598a7b470f47e72be25
JKellie <jkellie@me.com>
2017-11-18 18:16:30 -0500

    load array method

--- src/edu/seminolestate/manageproperties/ManageProperties.java
+++ src/edu/seminolestate/manageproperties/ManageProperties.java
@@ -3,8 +3,10 @@ package edu.seminolestate.manageproperties;
 import edu.seminolestate.properties.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,9 +25,13 @@ public class ManageProperties {
 	private static final int EXIT = 6;
 	
 	private static final String FILE_NAME = "properties.dat";
-	static File outputFile = new File(FILE_NAME); 
-
+	private static File outputFile = new File(FILE_NAME);
+	private static File objectFile = new File(FILE_NAME);
+	
 	public static void main(String[] args) {
+		if (objectFile.exists())
+			loadArrayList();
+		
 		int choice = 0;
 		do {
 			choice = menu();
@@ -181,6 +187,24 @@ public class ManageProperties {
 				e.printStackTrace();
 				System.exit(-1);
 			}
+		}
+	}
+	
+	public static void loadArrayList() {
+		try (FileInputStream fiStream = new FileInputStream(objectFile);
+	        	 ObjectInputStream oiStream = new ObjectInputStream(fiStream); ) {
+	        	while (true) {
+	        		Property property = (Property) oiStream.readObject();
+	        		properties.add(property);
+	        	}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Serialization version error.");
+			System.out.println("Shutting down");
+			e.printStackTrace();
+			System.exit(-1);
 		}
 	}
 	
